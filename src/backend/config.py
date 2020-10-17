8d9626c8cc2cccfee475c242a30357b6aaa2700d
Ashish D'Souza <sudouser512@gmail.com>
2020-10-16 21:39:07 -0400

    Added config.py

---
+++ src/backend/config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# config.py
+
+if __name__ == '__main__':
+    exit(0)
+
+
+# Flask Server
+IP = '0.0.0.0'  # Flask server IP address
+PORT = 80  # Flask server port
+
+# DataStax Astra
+DB_USERNAME = ''  # DataStax Astra database username
+DB_PASSWORD = ''  # DataStax Astra database password
+DB_BUNDLE_LOCATION = ''  # Path to DataStax Astra secure connect bundle zip file
