e6f5a987c4f610d44f352241da197af70d4f3dd3
therealsharath <palathingal@gatech.edu>
2020-10-16 21:49:39 -0400

    frontend skeleton + google login

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Home from './components/Home.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
