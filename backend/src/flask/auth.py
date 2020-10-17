3146b77c6c1d31e9bc344351bcf99eab4a1afdcd
Ashish D'Souza <sudouser512@gmail.com>
2020-10-17 13:28:22 -0400

    Update

--- backend/src/flask/auth.py
+++ backend/src/flask/auth.py
@@ -2,8 +2,13 @@
 # auth.py
 
 import os
-from flask import request, jsonify
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from functools import wraps
+from flask import request, jsonify
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from config import DB_USERNAME, DB_PASSWORD, DB_BUNDLE_LOCATION
 
 
 def authenticate(func):
@@ -13,6 +18,13 @@ def authenticate(func):
         if request.json:
             uid = request.json.get('uid')
             email = request.json.get('email')
+
+            auth_provider = PlainTextAuthProvider(DB_USERNAME, DB_PASSWORD)
+            cluster = Cluster(cloud={'secure_connect_bundle': DB_BUNDLE_LOCATION}, auth_provider=auth_provider)
+            conn = cluster.connect()
+            users = conn.execute('''
+            ''')
+
             return func(*args, **kwargs)
         return jsonify({'success': False, 'authenticated': False}), 401
     return wrapper
