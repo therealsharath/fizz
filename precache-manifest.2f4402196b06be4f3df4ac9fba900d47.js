edc239f18c1fe4c77a8ed87a0ac6aad3a85e109b
therealsharath <palathingal@gatech.edu>
2020-10-18 09:11:41 -0400

    Updates

---
+++ precache-manifest.2f4402196b06be4f3df4ac9fba900d47.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d6abfbcdf18b0541e3c17f8b93f14b",
+    "url": "/hackgt7/index.html"
+  },
+  {
+    "revision": "e2e1595eba3b36dd9f53",
+    "url": "/hackgt7/static/css/2.08a81a33.chunk.css"
+  },
+  {
+    "revision": "ada250fa425200d7dc7c",
+    "url": "/hackgt7/static/css/main.95be1305.chunk.css"
+  },
+  {
+    "revision": "e2e1595eba3b36dd9f53",
+    "url": "/hackgt7/static/js/2.7ebf31b8.chunk.js"
+  },
+  {
+    "revision": "fbb22318fd75e7e20daef4d3f59b1919",
+    "url": "/hackgt7/static/js/2.7ebf31b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada250fa425200d7dc7c",
+    "url": "/hackgt7/static/js/main.79a1fe01.chunk.js"
+  },
+  {
+    "revision": "1d310434405bb1bb746c",
+    "url": "/hackgt7/static/js/runtime-main.837eaa26.js"
+  },
+  {
+    "revision": "0b1e5c66ff3a8182adf594c54f08f630",
+    "url": "/hackgt7/static/media/G.0b1e5c66.png"
+  },
+  {
+    "revision": "44cf8c8a6c1b02849f32468d881d17ef",
+    "url": "/hackgt7/static/media/chat-bg.44cf8c8a.jpg"
+  },
+  {
+    "revision": "4b93b0198baa8d1828e916f0a85ef1a8",
+    "url": "/hackgt7/static/media/fizzlogo.4b93b019.png"
+  },
+  {
+    "revision": "158baf873717334fe9a695e58ee3fcc4",
+    "url": "/hackgt7/static/media/login.158baf87.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f67cb666d93f9dacbfefde18dbd3b74b.js"
+  "/hackgt7/precache-manifest.2f4402196b06be4f3df4ac9fba900d47.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hackgt7/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
