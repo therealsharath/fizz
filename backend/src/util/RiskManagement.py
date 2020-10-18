105ef6fb2d7afc8c44285417f3d2d50dce46b313
bobbybloomquist <52797617+bobbybloomquist@users.noreply.github.com>
2020-10-18 06:07:33 -0400

    Add ROI functionality, update portfolio analysis

--- backend/src/util/RiskManagement.py
+++ backend/src/util/RiskManagement.py
@@ -9,9 +9,9 @@ from BlackRockData import getRisk
 # totalCapital - total amount the user has to invest
 # investedCapital - amount of capital invested currently
 # unusedCapital - amount of capital not invested currently (total - invested)
-# portfolio - maps asset, date bought pairs to the number bought, price when bought, stop loss value
-#             or strike price of downside put, and industry
-# portfolio example: portfolio[(asset, date)] = (number bought, price when bought, lowest loss price, industry)
+# portfolio - maps asset, date bought pairs to the number bought, price when bought, and stop loss value
+#             or strike price of downside put
+# portfolio example: portfolio[(asset, date)] = (number bought, price when bought, lowest loss price)
 
 ################################################################################
 # Calculate risk management basics (for portfolio analysis)
@@ -20,7 +20,7 @@ from BlackRockData import getRisk
 # assetID parameter here is an asset name, date bought tuple
 def calcInvestedCapital(portfolio):
     investedCapital = 0
-    for assetID in portfolio:
+    for assetID in portfolio.keys():
         investedCapital += portfolio[assetID][0] * portfolio[assetID][1]
     return investedCapital
 
@@ -40,16 +40,27 @@ def percentRisk(amountBought, priceBought, riskManagementPrice, totalCapital):
 # assetID parameter here is an asset name, date bought tuple
 def onePercentRule(portfolio, totalCapital):
     risks = {}
-    for assetID in portfolio:
+    for assetID in portfolio.keys():
         assetDetails = portfolio[assetID]
         risks[assetID] = percentRisk(assetDetails[0], assetDetails[1], assetDetails[2], totalCapital)
     return risks
 
-# what should we diversify by? by company, industry, type of asset, mutual funds?
-# Maybe do all of them and take a weighted average to return a diversification
-# index, which could indicate how diverse the portfolio is.
-def diverse(portfolio):
-    pass
+# gets the total return on investment with respect to the given portfolio, as a percentage
+def returnOnInvestment(portfolio):
+    netReturn = 0
+    cost = 0
+    for assetID in portfolio.keys():
+        assetDetails = portfolio[assetID]
+        assetName = assetID[0]
+        quantity = assetDetails[0]
+        prevPrice = assetDetails[1]
+        minLoss = assetDetails[2]
+        currPrice = get105prices(assetName)[0]
+
+        netReturn += (max(currPrice, minLoss) - prevPrice) * quantity
+        cost += prevPrice * quantity
+
+    return 100 * netReturn / cost
 
 
 ################################################################################
@@ -229,25 +240,32 @@ that you risk.\n".format(risked = capitalRisked * 100, name = assetName)
     return string
 
 # analyzes the given portfolio with unused capital and some (2 or 3) of the five basic rules of risk management
+# as well as the current return on investment
 def analyzePortfolio(portfolio, totalCapital):
     investedCapital = calcInvestedCapital(portfolio)
     unusedCapital = totalCapital - investedCapital
 
+    currentROI = returnOnInvestment(portfolio)
+
     riskyAssets = []
     risks = onePercentRule(portfolio, totalCapital)
-    for assetID in keyset(risks):
+    for assetID in risks.keys():
         capitalRisked, riskManaged = risks[assetID]
         if capitalRisked > 0.01 or not riskManaged:
             riskyAssets.append(assetID[0], capitalRisked, riskManaged)
 
-    if unusedCapital > 0:
-        returnString = "Looks like you have some capital that you aren't using. Ask me about buying certain stocks.\n"
-    else:
-        returnString = "Good job utilizing all of your capital!\n"
+    returnString = ""
 
     if len(riskyAssets) == 0:
-        returnString += "It also looks like you have mitigated much of your risks. Great job!"
+        returnString += "It looks like you have mitigated many of your risks. Great job!"
     else:
         returnString = populateStringWithRisks(returnString, riskyAssets)
+
+    returnString += "\nYour portfolio currently has a return on investment of {roi}%.".format(currentROI)
+
+    if unusedCapital > 0:
+        returnString += "\nLooks like you have some capital that you aren't using. Ask me about buying certain stocks."
+    else:
+        returnString += "\nGood job utilizing all of your capital!"
 
     return returnString
