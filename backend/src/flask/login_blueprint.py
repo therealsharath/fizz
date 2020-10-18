042678dc6791fef7970d42bb6d8ea82821219d5d
Ashish D'Souza <sudouser512@gmail.com>
2020-10-17 21:36:14 -0400

    Added Dialogflow

--- backend/src/flask/login_blueprint.py
+++ backend/src/flask/login_blueprint.py
@@ -2,8 +2,13 @@
 # login_blueprint.py
 
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from flask import Blueprint, request, jsonify
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
 from auth import authenticate
+from config import DB_USERNAME, DB_PASSWORD, DB_BUNDLE_LOCATION
 
 
 login_blueprint = Blueprint('login_blueprint', __name__, template_folder=os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'templates'), static_folder=os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static'))
@@ -14,3 +19,15 @@ login_blueprint = Blueprint('login_blueprint', __name__, template_folder=os.path
 @authenticate
 def post_login():
     return jsonify({'success': True, 'authenticated': True}), 200
+
+
+# Create user
+@login_blueprint.route('/user/create', methods=['POST'])
+def post_create_user():
+    if not request.json:
+        return jsonify({'success': False, 'authenticated': False}), 400
+
+    uid = request.json.get('userId')
+    email = request.json.get('userEmail')
+    if not uid or not email:
+        return jsonify({'success': False, 'authenticated': False}), 400
