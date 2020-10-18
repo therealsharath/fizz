248f67f322e72fafb1e16f6e544acb747f17f76a
Ashish D'Souza <sudouser512@gmail.com>
2020-10-18 01:06:22 -0400

    Added chatbot functionality in back-end

--- backend/src/config.py
+++ backend/src/config.py
@@ -17,5 +17,9 @@ DB_USERNAME = ''  # DataStax Astra database username
 DB_PASSWORD = ''  # DataStax Astra database password
 DB_BUNDLE_LOCATION = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'db', 'secure-connect-hackgt-7.zip')  # Path to DataStax Astra secure connect bundle zip file
 
+# Dialogflow
+DIALOGFLOW_PROJECT_ID = ''  # Project ID of Dialogflow application
+
 # API Keys
+GOOGLE_APPLICATION_CREDENTIALS = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'hackgt7-google.json')  # Location of JSON file containing Google application credentials
 FMP_API_KEY = ''  # Financial Modeling Prep API Key
