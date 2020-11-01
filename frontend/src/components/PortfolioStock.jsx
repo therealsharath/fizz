62f0dae4b720347c241b61c36541494feb4dde98
therealsharath <palathingal@gatech.edu>
2020-11-01 06:00:58 -0500

    Minor CSS fixes

--- frontend/src/components/PortfolioStock.jsx
+++ frontend/src/components/PortfolioStock.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { lighten, makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -10,7 +9,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
 import Paper from '@material-ui/core/Paper';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
@@ -103,37 +101,12 @@ EnhancedTableHead.propTypes = {
   rowCount: PropTypes.number.isRequired,
 };
 
-const useToolbarStyles = makeStyles((theme) => ({
-  root: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-  },
-  highlight:
-    theme.palette.type === 'light'
-      ? {
-          color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-        }
-      : {
-          color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
-        },
-  title: {
-    flex: '1 1 100%',
-  },
-}));
 
 const EnhancedTableToolbar = (props) => {
-  const classes = useToolbarStyles();
   const numSelected = props.numSelected;
 
   return (
-    <Toolbar
-      className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      {numSelected > 0 ? (
+      numSelected > 0 ? (
         <Tooltip title="Delete" onClick={() => {
             props.deleteStock()
             props.setSelected([])
@@ -142,8 +115,7 @@ const EnhancedTableToolbar = (props) => {
             <DeleteIcon/>
           </IconButton>
         </Tooltip>
-      ) : null }
-    </Toolbar>
+      ) : null 
   );
 };
 
